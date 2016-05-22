b70589401ba3e2aa9d871b98eb537e24bbb83dbe
Björn Frantzén <bjorn.frantzen@r2m.se>
2016-05-22 13:05:38 +0200

    Kmeans exercise completed

--- k-means/README.md
+++ k-means/README.md
@@ -1,11 +1,36 @@
-Instructions
-------------
 
-In this lab we will use the K-Means algorithm to reduce the colors in an image.
+K-Means clustering
+------------------
 
-Apart from python we we use two external libraries:
+# Introduction
 
-h2. Numpy
+K-means is an unsupervised clustering algorithm which is used to group a number of points into
+K groups (or "clusters"). The process is iterative, starting at (usually) random points and then
+iteratively improving the center of each cluster, called the "centriod", until the centroid 
+stabilizes. K-means gives best result when the data points are separated in distinct clusters.
+K-means main weakness is that the number of clusters must be known beforehand.
+
+This demonstration will help to visualize how the K-Means clustering algorithm works:
+http://stanford.edu/class/ee103/visualizations/kmeans/kmeans.html
+
+# Excercise
+
+In this lab we will use the K-Means algorithm to reduce the colors in an image. The alorithm will
+take the image (data) as well as the number of colors you want to use in the resulting image (K)
+and will then be group the image pixels into K groups, returningthe calculated centroids 
+(i.e. the colors to use) and a list of lists containing which cluster each pixel belongs to.
+
+Use the code in kmeans.py as a starting base. Locate the TODO comments and follow the instructions
+to create the needed code.
+
+A new image will be generated using OpenCV and displayed on the screen. Close the image by
+pressing the any key while having the image in focus!
+
+One possible solution exists in kmeans-solution.py. Please don't peek unless you get stuck! :)
+
+## Apart from python we we use two external libraries:
+
+### Numpy
 
 The central feature of Numpy is the array object class. Arrays are similar to lists in Python,
 except that every element of an array must be of the same type, typically a numeric type like
@@ -19,7 +44,7 @@ which can be displayed.
 For more information, read:
 http://www.engr.ucsb.edu/~shell/che210d/numpy.pdf
 
-h2. OpenCV
+### OpenCV
 
 OpenCV is an Open Source Computer Vision library which contains implementations of several
 Machine Learning algorithms, as well as functionality to display and manipulate images.
@@ -29,4 +54,3 @@ http://docs.opencv.org/2.4/
 
 In this lab we will use the k-means algorithm of OpenCV. You can find it documented here:
 http://docs.opencv.org/2.4/modules/core/doc/clustering.html
-
--- k-means/kmeans-solution.py
+++ k-means/kmeans-solution.py
@@ -1,47 +1,68 @@
 import numpy
 import cv2
 
-#Enable debug printing of larger part of numpy array
-numpy.set_printoptions(edgeitems=32)
-
 # Read the image which should be processed. It will be stored in memory as
 # an array of arrays of arrays. In the case of an image of size 32x32 it will contain
 # 32 columns -> 32 Rows -> Pixel [R,B,G]
-img = cv2.imread('img-32x32.jpg')
+# Thre images are available, you can raplce them with your own if you want
+#  img-640x640.jpg - Image of a cat
+#  img-32x32.jpg   - Small image used for debugging
+#  red.jpg         - Small single color image, used for debugging
+img = cv2.imread('img-640x640.jpg')
 
+# TODO
 # Reshape the image into an array of arrays, each row containing the
 # value of one pixel split inte Red, Green and Blue color. In the case 
 # of an image of size 32x32 it must contain 1024 Rows -> Pixel [R,B,G]
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.reshape.html
 Z = img.reshape((-1, 3))
 
+# TODO
 # Use numpy to transform the binary image to an array of arrays of float32,
-# each array containing the Red, Blue and Green representation of the image
+# each array containing the Red, Blue and Green representation of the image.
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.ndarray.astype.html
 Z = numpy.float32(Z)
 
+# TODO
 # Run the cv2.kmeans algorithm, where K should be the number of colors
 # you want in the processed image
+# http://docs.opencv.org/2.4/modules/core/doc/clustering.html
 ret, label, center = cv2.kmeans(
+  # Input data
   data=Z,
-  K=4,
+  # Number of iterations
+  K=?4,
   bestLabels=None,
+  # Algorithm termination criteria. when to stop iterating (because centroids
+  # are not moving anymore) and maximum iterations
+  # see documentation for more information
   criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0), 
-  attempts=10, 
-  flags=cv2.KMEANS_RANDOM_CENTERS)
+  # Number of attempts using different initial labelings.
+  attempts=10,
+  # How to select initial centers
+  flags=cv2.KMEANS_PP_CENTERS)
 
-print "Reshaped Input Data"
-print Z
-print "K-Means calculated labels"
-print label
-print "K-Means calculated colors"
-print center
+# Set to 1 to enable printing of debug information
+if 0:
+  numpy.set_printoptions(edgeitems=32) #Enable debug printing of larger part of numpy array
+  print "Reshaped Input Data"
+  print Z
+  print "K-Means calculated labels"
+  print label
+  print "K-Means calculated colors"
+  print center
 
+# TODO
 # Use numpy to convert the float32 array to an uint8 array
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.ndarray.astype.html
 center = numpy.uint8(center)
 
 # Map the calculated colors using the the labels into a new image
 res = center[label.flatten()]
 
+# TODO
 # Restore the original image dimensions
+# http://docs.scipy.org/doc/numpy-1.10.1/reference/generated/numpy.reshape.html
 res = res.reshape(img.shape)
 
 # This code will display the result. Close the image by clicking in the image and
